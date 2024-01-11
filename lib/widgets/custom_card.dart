b6a20b3fea3593f48dd0b6ef815fcec3bf454b81
Kseniia <email@ks.ks.krom@gmail.com>
2024-01-11 04:45:11 +0200

    first commit

---
+++ lib/widgets/custom_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget CustomCard({required child}) {
+  return Card(
+    elevation: 5,
+    shadowColor: Colors.orange[300],
+    color: Colors.black38,
+    child: Container(width: 300, height: 200,
+    child:child)
+  );
+}
